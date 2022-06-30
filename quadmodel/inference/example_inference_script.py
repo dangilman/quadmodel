e9b459aa6ae411b37a757bf1bb88cc38b235f6ed
Daniel Gilman <gilmanda@ucla.edu>
2022-06-30 10:07:10 -0400

    update

--- quadmodel/inference/example_inference_script.py
+++ quadmodel/inference/example_inference_script.py
@@ -5,24 +5,26 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 
-lens_name = 'RXJ0911'
+lens_name = 'B1422'
 output_path = os.getenv('HOME') + '/Code/quadmodel/notebooks/'+lens_name+'ULDMtest/'
 job_index = 1
-n_keep = 20
+n_keep = 2
+tolerance = 0.05
+
 realization_priors = {}
-realization_priors['PRESET_MODEL'] = 'ULDM'
+realization_priors['PRESET_MODEL'] = 'ULDM' # see inference/realization_setup
+
 #ULDM specific parameters
-realization_priors['log10_m_uldm'] = ['UNIFORM', -22., -16.5]
-realization_priors['uldm_plaw'] = ['UNIFORM', 0.2, 0.5]
-realization_priors['log10_fluc_amplitude'] = ['UNIFORM', -2.5, -0.5]
+#ULDM specific parameters
+realization_priors['log10_m_uldm'] = ['UNIFORM', -22.5, -16.5]
+realization_priors['flucs'] = ['FIXED', False]
+realization_priors['uldm_plaw'] = ['FIXED', 0.2]
+
 # general parameters
 realization_priors['sigma_sub'] = ['UNIFORM', 0.0, 0.1]
 realization_priors['LOS_normalization'] = ['UNIFORM', 0.8, 1.2]
-realization_priors['power_law_index'] = ['UNIFORM', -1.95, -1.85]
-# 15, -0.3
-# realization_priors['c_power'] = ['FIXED', -0.17]
-# realization_priors['c_scale'] = ['FIXED', 100]
-tolerance = 1e10
+realization_priors['power_law_index'] = ['FIXED', -1.95, -1.85]
+verbose = True
 
 forward_model(output_path, job_index, lens_name, n_keep, realization_priors, tolerance=tolerance,
-                  verbose=True, readout_steps=4, test_mode=True)
+              verbose=verbose, readout_steps=2, test_mode=True)
