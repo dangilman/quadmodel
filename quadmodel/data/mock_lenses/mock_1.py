bfae8d47f65970749e08ee243439fb9ccd00f866
Daniel Gilman <gilmanda@ucla.edu>
2022-07-29 00:12:53 -0400

    edit mock1

--- quadmodel/data/mock_lenses/mock_1.py
+++ quadmodel/data/mock_lenses/mock_1.py
@@ -1,42 +1,27 @@
 from quadmodel.data.quad_base import Quad
 
-class Mock_1_MIDIR(Quad):
-
-    def __init__(self, sourcemodel_type='midIR_Gaussian',
-                 macromodel_type='EPL_FREE_SHEAR_MULTIPOLE'):
-
-        zlens = 0.4
-        self.zlens = zlens
-        zsource = 1.2
-        x = [ 0.9109, -0.042 ,  0.7291, -0.7783]
-        y = [-0.5205,  0.9844,  0.738 , -0.4425]
-        m = [0.6413, 1.0071, 0.9886, 0.343 ]
-        delta_m = [0.02] * 4
-        delta_xy = [0.005] * 4
-        keep_flux_ratio_index = [0, 1, 2]
-
-        kwargs_macromodel = {'shear_amplitude_min': 0.01, 'shear_amplitude_max': 0.1}
-
-        super(Mock_1_MIDIR, self).__init__(zlens, zsource, x, y, m, delta_m, delta_xy, sourcemodel_type, {}, macromodel_type,
-                                    kwargs_macromodel, keep_flux_ratio_index)
-
-class Mock_1_NL(Quad):
+class Mock1(Quad):
 
     def __init__(self, sourcemodel_type='NARROW_LINE_Gaussian',
-                 macromodel_type='EPL_FREE_SHEAR_MULTIPOLE'):
+                 macromodel_type='EPL_FIXED_SHEAR'):
 
-        zlens = 0.4
-        self.zlens = zlens
-        zsource = 1.2
-        x = [0.9109, -0.042, 0.7291, -0.7783]
-        y = [-0.5205, 0.9844, 0.738, -0.4425]
-        m = [0.6289, 0.9843, 0.8869, 0.3381]
-        delta_m = [0.04] * 4
-        delta_xy = [0.005] * 4
+        """
+
+        """
+        zlens = 0.5
+        zsource = 2.0
+        x = [1.10502282,-0.08588209, 0.57245819, -0.58034621]
+        y = [-0.3520062, 1.05155373, 0.85025812, -0.46824808]
+        m = [0.52862854, 0.98170444, 1., 0.20642608]
+        delta_m = [0.0001, 0.0001, 0.0001, 0.0001]
+        delta_xy = [0.001] * 4
         keep_flux_ratio_index = [0, 1, 2]
 
-        kwargs_macromodel = {'shear_amplitude_min': 0.01, 'shear_amplitude_max': 0.1}
+        self.log10_host_halo_mass = 13.3
+        self.log10_host_halo_mass_sigma = 0.3
 
-        super(Mock_1_NL, self).__init__(zlens, zsource, x, y, m, delta_m, delta_xy, sourcemodel_type, {},
-                                           macromodel_type,
-                                           kwargs_macromodel, keep_flux_ratio_index)
+        kwargs_macromodel = {'shear_amplitude_min': 0.02, 'shear_amplitude_max': 1.6}
+
+        super(Mock1, self).__init__(zlens, zsource, x, y, m, delta_m, delta_xy, sourcemodel_type, {}, macromodel_type,
+                                    kwargs_macromodel, keep_flux_ratio_index)
+
