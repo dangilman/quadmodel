944e8367946925607d47bae465eb4a751180bff7
Daniel Gilman <gilmanda@ucla.edu>
2023-02-08 16:46:40 -0500

    add chi2 to simulation_container class

--- quadmodel/Solvers/source_optimization.py
+++ quadmodel/Solvers/source_optimization.py
@@ -1,12 +1,7 @@
 import numpy as np
-import lenstronomy.Util.simulation_util as sim_util
-import lenstronomy.Util.image_util as image_util
 import os
-from lenstronomy.ImSim.image_model import ImageModel
-from lenstronomy.PointSource.point_source import PointSource
 import sys
 import pickle
-from lenstronomy.LightModel.light_model import LightModel
 from lenstronomy.Data.imaging_data import ImageData
 from lenstronomy.Data.psf import PSF
 from lenstronomy.Workflow.fitting_sequence import FittingSequence
@@ -14,17 +9,28 @@ from copy import deepcopy
 from lenstronomy.Plots.model_plot import ModelPlot
 
 N_jobs = 2
-job_index = int(sys.argv[1])
-path_to_simulation_output = os.getenv('HOME')+'/Code/quadmodel/notebooks/fluxratio_arc_inference/inference_output/job_'+str(job_index)+'/'
-path_to_data = os.getenv('HOME')+'/Code/quadmodel/notebooks/fluxratio_arc_inference/'
+cluster = 'LAPTOP'
+filename_suffix = ''
 
+if cluster=='LAPTOP':
+    job_index = 1131
+    path_to_simulation_output = os.getenv('HOME')+'/Code/quadmodel/notebooks/fluxratio_arc_inference/inference_output/job_'+str(job_index)+'/'
+    path_to_data = os.getenv('HOME')+'/Code/quadmodel/notebooks/fluxratio_arc_inference/'
+elif cluster=='NIAGARA':
+    job_index = int(sys.argv[1])
+    path_to_simulation_output = os.getenv('SCRATCH') + '/chains/extended_image_quad_sim/job_' + str(job_index) + '/'
+    path_to_data = os.getenv('HOME') + '/mock_data/'
+else:
+    raise Exception('cluster must be either LAPTOP or NIAGARA')
+
+include_shapelets = False
 n_max = 4
 n_threads = 8
 
-n_pso_particles = 2
-n_pso_iterations = 2
-n_burn = 2
-n_run = 2
+n_pso_particles = 100
+n_pso_iterations = 100
+n_burn = 100
+n_run = 300
 
 for idx in range(1, N_jobs+1):
 
@@ -36,6 +42,9 @@ for idx in range(1, N_jobs+1):
         print('could not find simulation output file '+path_to_simulation_output + 'simulation_output_'+str(idx))
         continue
 
+#    if os.path.exists(path_to_simulation_output+'chi2_'+str(idx)+'.txt'):
+#        print('chi2 computation already performed for file '+str(job_index))
+
     f = open(path_to_data+'simulated_lens_data', 'rb')
     hst_data = pickle.load(f)
     f.close()
@@ -43,10 +52,14 @@ for idx in range(1, N_jobs+1):
     x_image, y_image = simulation_output.data.x, simulation_output.data.y
     fluxes = simulation_output.data.m
     lens_system = simulation_output.lens_system
-    lensmodel, kwargs_lens = lens_system.get_lensmodel()
-    source_x, source_y = lensmodel.ray_shooting(x_image, y_image, kwargs_lens)
+    lensmodel, kwargs_lens_init = lens_system.get_lensmodel()
+    source_x, source_y = lensmodel.ray_shooting(x_image, y_image, kwargs_lens_init)
     source_x = np.mean(source_x)
     source_y = np.mean(source_y)
+    lensmodel_macro, kwargs_macro = lens_system.get_lensmodel(include_substructure=False)
+    n_macro = len(kwargs_macro)
+    lens_model_list_macro = lensmodel_macro.lens_model_list
+    lens_model_list_halos = lensmodel.lens_model_list[n_macro:]
 
     ###################################
     """"The rest runs lenstronomy only"""
@@ -55,58 +68,6 @@ for idx in range(1, N_jobs+1):
     source_brightness_intrinsic = 100.0
     kwargs_ps = [{'ra_image': x_image, 'dec_image': y_image,
                   'point_amp': fluxes * source_brightness_intrinsic}]
-    lensmodel, kwargs_lens = lens_system.get_lensmodel()
-    lensmodel_macro, kwargs_macro = lens_system.get_lensmodel(include_substructure=False)
-
-    # data specifics
-    background_rms = 0.5  # background noise per pixel
-    exp_time = 100  # exposure time (arbitrary units, flux per pixel is in units #photons/exp_time unit)
-    deltaPix = 0.05  # pixel size in arcsec (area per pixel = deltaPix**2)
-    window_size = 4.0
-    numPix = int(window_size / deltaPix)
-    fwhm = 0.1  # full width half max of PSF
-
-    # PSF specification
-    kwargs_data = sim_util.data_configure_simple(numPix, deltaPix, exp_time, background_rms)
-    data_class = ImageData(**kwargs_data)
-    kwargs_psf = {'psf_type': 'GAUSSIAN', 'fwhm': fwhm, 'pixel_size': deltaPix, 'truncation': 5}
-    psf_class = PSF(**kwargs_psf)
-
-    lens_model_list_macro_data = ['EPL', 'SHEAR']
-    kwargs_lens_macro_data = [{'theta_E': 1.0, 'center_x': 0.0, 'center_y': 0.0, 'e1': 0.25, 'e2': -0.1, 'gamma': 2.09},
-                              {'gamma1': 0.013, 'gamma2': 0.06}]
-
-    # list of light profiles (for lens and source)
-    # 'SERSIC': spherical Sersic profile
-    lens_light_model_list = ['SERSIC_ELLIPSE']
-    kwargs_sersic = {'amp': 800, 'R_sersic': 0.6, 'n_sersic': 4.0, 'e1': 0.1, 'e2': -0.05, 'center_x': 0.0,
-                     'center_y': 0.0}
-    kwargs_lens_light = [kwargs_sersic]
-    lens_light_model_class = LightModel(light_model_list=lens_light_model_list)
-
-    # 'SERSIC_ELLIPSE': elliptical Sersic profile
-    source_model_list = ['SERSIC_ELLIPSE']
-    kwargs_sersic_ellipse = {'amp': 3000., 'R_sersic': 0.04, 'n_sersic': 3.2, 'center_x': source_x,
-                             'center_y': source_y, 'e1': 0.1, 'e2': 0.15}
-    kwargs_source = [kwargs_sersic_ellipse]
-    source_model_class = LightModel(light_model_list=source_model_list)
-
-    # note: the relative magnification of point sources is not used as constraints in the fitting in the default settings of lenstronomy.
-    # you can set this constraint with the keyword 'fixed_magnification_list' (see next block). The images are treated otherwise as separate linear amplitudes that are constraint independently of each other.
-    point_source_list = ['LENSED_POSITION']
-    point_source_class = PointSource(point_source_type_list=point_source_list, fixed_magnification_list=[False])
-    kwargs_numerics = {'supersampling_factor': 1, 'supersampling_convolution': False}
-    imageModel = ImageModel(data_class, psf_class, lensmodel, source_model_class,
-                            lens_light_model_class,
-                            point_source_class, kwargs_numerics=kwargs_numerics)
-    image_sim = imageModel.image(kwargs_lens, kwargs_source, kwargs_lens_light, kwargs_ps)
-    poisson = image_util.add_poisson(image_sim, exp_time=exp_time)
-    bkg = image_util.add_background(image_sim, sigma_bkd=background_rms)
-    image_sim = image_sim + bkg + poisson
-
-    n_macro = len(kwargs_macro)
-    lens_model_list_macro = lensmodel_macro.lens_model_list
-    lens_model_list_halos = lensmodel.lens_model_list[n_macro:]
 
     kwargs_data = {'image_data': hst_data.image_data,
                    'background_rms': hst_data.background_rms,
@@ -126,11 +87,16 @@ for idx in range(1, N_jobs+1):
     psf_class = PSF(**kwargs_psf)
 
     lens_model_list = lens_model_list_macro + lens_model_list_halos
-    source_model_list = ['SERSIC_ELLIPSE']
-    if n_max>0:
-        source_model_list += ['SHAPELETS']
 
+    source_model_list = ['SERSIC_ELLIPSE']
+    kwargs_source_init = [
+        {'amp': 200, 'R_sersic': 0.2, 'n_sersic': 3.0, 'e1': 0.05, 'e2': 0.05, 'center_x': 0.0, 'center_y': 0.0}]
+    if include_shapelets:
+        source_model_list += ['SHAPELETS']
     lens_light_model_list = ['SERSIC_ELLIPSE']
+    kwargs_lens_light_init = [{'amp': 3000., 'R_sersic': 0.04, 'n_sersic': 3.2, 'center_x': source_x,
+                               'center_y': source_y, 'e1': 0.1, 'e2': 0.15}]
+
     point_source_list = ['LENSED_POSITION']
 
     kwargs_model = {'lens_model_list': lens_model_list,
@@ -139,6 +105,7 @@ for idx in range(1, N_jobs+1):
                     'point_source_model_list': point_source_list,
                     'additional_images_list': [False],
                     'fixed_magnification_list': [False],
+                    'fixed_lens_model': True
                     # list of bools (same length as point_source_type_list). If True, magnification ratio of point sources is fixed to the one given by the lens model
                     }
 
@@ -148,8 +115,7 @@ for idx in range(1, N_jobs+1):
 
     kwargs_constraints = {'joint_source_with_point_source': [[0, 0]],
                           'num_point_source_list': [4],
-                          'solver_type': 'PROFILE_SHEAR',
-                          'fixed_lens_model': True
+                          'solver_type': 'PROFILE_SHEAR'
                           }
 
     prior_lens = None
@@ -160,7 +126,7 @@ for idx in range(1, N_jobs+1):
                          'force_no_add_image': True,
                          'source_marg': False,
                          'image_position_uncertainty': 0.004,
-                         'check_matched_source_position': True,
+                         'check_matched_source_position': False,
                          'source_position_tolerance': 0.001,
                          'source_position_sigma': 0.001,
                          'prior_lens': prior_lens,
@@ -174,44 +140,55 @@ for idx in range(1, N_jobs+1):
     kwargs_data_joint = {'multi_band_list': multi_band_list, 'multi_band_type': 'multi-linear'}
 
     # initial guess of non-linear parameters, we chose different starting parameters than the truth #
-    shapelets_init = {'amp': 1.0, 'beta': 1e-1, 'n_max': n_max, 'center_x': 0.0, 'center_y': 0.0}
-    shapelets_sigma = {'amp': 0.5, 'beta': 0.2, 'n_max': 1.0, 'center_x': 0.1, 'center_y': 0.1}
-    shapelets_min = {'amp': 0.00001, 'beta': 1e-10, 'n_max': 1.0, 'center_x': -1.0, 'center_y': -1.0}
-    shapelets_max = {'amp': 1000.0, 'beta': 100.0, 'n_max': 10.0, 'center_x': 1.0, 'center_y': 1.0}
+    shapelets_init = [{'amp': 1.0, 'beta': 1e-1, 'n_max': n_max, 'center_x': 0.0, 'center_y': 0.0}]
+    shapelets_sigma = [{'amp': 0.5, 'beta': 0.2, 'n_max': 1.0, 'center_x': 0.1, 'center_y': 0.1}]
+    shapelets_min = [{'amp': 0.00001, 'beta': 1e-10, 'n_max': 1.0, 'center_x': -1.0, 'center_y': -1.0}]
+    shapelets_max = [{'amp': 1000.0, 'beta': 100.0, 'n_max': 10.0, 'center_x': 1.0, 'center_y': 1.0}]
 
-    kwargs_lens_init = kwargs_lens
-    kwargs_source_init = kwargs_source + [shapelets_init]
-    kwargs_lens_light_init = kwargs_lens_light
+    kwargs_source_init = [{'amp': 3000., 'R_sersic': 0.04, 'n_sersic': 3.2, 'center_x': source_x,
+                         'center_y': source_y, 'e1': 0.1, 'e2': 0.15}]
+    kwargs_lens_light_init = [{'amp': 800, 'R_sersic': 0.6, 'n_sersic': 4.0, 'e1': 0.1,
+                              'e2': -0.05, 'center_x': 0.0, 'center_y': 0.0}]
+
+    if include_shapelets:
+        kwargs_source_init += shapelets_init
     kwargs_ps_init = [{'ra_image': x_image, 'dec_image': y_image}]
 
     # initial spread in parameter estimation #
-    kwargs_lens_sigma = deepcopy(kwargs_lens)
-    kwargs_source_sigma = [{'R_sersic': 0.2, 'n_sersic': 0.5, 'e1': 0.4, 'e2': 0.4, 'center_x': .1, 'center_y': 0.1}] + [shapelets_sigma]
+    kwargs_lens_sigma = deepcopy(kwargs_lens_init)
+    kwargs_source_sigma = [{'R_sersic': 0.1, 'n_sersic': .5, 'e1': 0.4, 'e2': 0.4, 'center_x': .1, 'center_y': 0.1}]
+    if include_shapelets:
+        kwargs_source_sigma += shapelets_sigma
     kwargs_lens_light_sigma = [
         {'R_sersic': 0.1, 'n_sersic': 0.2, 'e1': 0.4, 'e2': 0.4, 'center_x': .1, 'center_y': 0.1}]
     kwargs_ps_sigma = [{'ra_image': [1e-5] * 4, 'dec_image': [1e-5] * 4}]
 
     # hard bound lower limit in parameter space #
-    kwargs_lower_lens = deepcopy(kwargs_lens)
+    kwargs_lower_lens = deepcopy(kwargs_lens_init)
     kwargs_lower_source = [{'R_sersic': 0.001, 'n_sersic': 0.5, 'e1': -0.5, 'e2': -0.5, 'center_x': -10,
-                            'center_y': -10}] + [shapelets_min]
+                            'center_y': -10}]
+    if include_shapelets:
+        kwargs_lower_source += shapelets_min
     kwargs_lower_lens_light = [
         {'R_sersic': 0.001, 'n_sersic': 0.5, 'e1': -0.5, 'e2': -0.5, 'center_x': -10, 'center_y': -10}]
     kwargs_lower_ps = [{'ra_image': -10 * np.ones_like(x_image), 'dec_image': -10 * np.ones_like(y_image)}]
 
     # hard bound upper limit in parameter space #
-    kwargs_upper_lens = deepcopy(kwargs_lens)
+    kwargs_upper_lens = deepcopy(kwargs_lens_init)
     kwargs_upper_source = [{'R_sersic': 10.0, 'n_sersic': 10.0, 'e1': 0.5, 'e2': 0.5, 'center_x': 10,
-                            'center_y': 10}] + [shapelets_max]
+                            'center_y': 10}]
+    if include_shapelets:
+        kwargs_upper_source += shapelets_max
     kwargs_upper_lens_light = [{'R_sersic': 10, 'n_sersic': 10.0, 'e1': 0.5, 'e2': 0.5, 'center_x': 10, 'center_y': 10}]
     kwargs_upper_ps = [{'ra_image': 10 * np.ones_like(x_image), 'dec_image': 10 * np.ones_like(y_image)}]
 
     # keeping parameters fixed
-    kwargs_lens_fixed = deepcopy(kwargs_lens)
+    kwargs_lens_fixed = deepcopy(kwargs_lens_init)
     index_shear = 1
     kwargs_lens_fixed[index_shear].update({'ra_0': 0, 'dec_0': 0})
-    kwargs_source_fixed = [{'center_x': source_x, 'center_y': source_y},
-                           {'center_x': source_x, 'center_y': source_y, 'n_max': n_max}]
+    kwargs_source_fixed = [{'center_x': source_x, 'center_y': source_y}]
+    if include_shapelets:
+        kwargs_source_fixed += [{'center_x': source_x, 'center_y': source_y, 'n_max': n_max}]
     kwargs_lens_light_fixed = [{}]
     kwargs_ps_fixed = [{}]
 
@@ -239,11 +216,12 @@ for idx in range(1, N_jobs+1):
     chain_list = fitting_seq.fit_sequence(fitting_kwargs_list)
     modelPlot = ModelPlot(multi_band_list, kwargs_model, kwargs_result, arrow_size=0.02, cmap_string="gist_heat")
     log_l = modelPlot._imageModel.likelihood_data_given_model(source_marg=False, linear_prior=None,
-                                                                 **kwargs_result)
+                                                              **kwargs_result)
     n_data = modelPlot._imageModel.num_data_evaluate
-    chi2 = 2*log_l/n_data
-
-    f = open(path_to_simulation_output+'chi2_'+str(idx)+'.txt','w')
+    chi2 = 2 * log_l / n_data
+    print('n_data: ', n_data)
+    print('log_l: ', log_l)
+    f = open(path_to_simulation_output + 'chi2_' + str(idx) + filename_suffix + '.txt', 'w')
     f.write(str(np.round(chi2, 6)))
     f.close()
 
--- quadmodel/inference/util.py
+++ quadmodel/inference/util.py
@@ -4,24 +4,31 @@ from copy import deepcopy
 
 class FullSimulationContainer(object):
 
-    def __init__(self, individual_simulations, parameters, magnifications):
+    def __init__(self, individual_simulations, parameters, magnifications, chi2_imaging_data=None):
 
         """
         A storage class for individual simulation containers
         :param individual_simulations: a list of SimulationOutputContainer classes
         :param parameters: a numpy array containing the samples accepted into the posterior
         :param magnifications a numpy array containing the magnifications corresponding to a particular set of parameters
+        :param chi2_imaging_data: a numpy array containing reduced chi2 of the lens and source light models given
+        the imaging data
         """
         self.simulations = individual_simulations
         self.parameters = parameters
         self.magnifications = magnifications
+        self.chi2_imaging_data = chi2_imaging_data
 
     @classmethod
     def join(cls, sim1, sim2):
         mags = np.vstack((sim1.magnifications, sim2.magnifications))
         params = np.vstack((sim1.parameters, sim2.parameters))
         simulations = [sim1, sim2]
-        combined = FullSimulationContainer(simulations, params, mags)
+        chi2 = None
+        if sim1.chi2_imaging_data is not None:
+            assert sim2.chi2_imaging_data is not None
+            chi2 = np.append(sim1.chi2_imaging_data, sim2.chi2_imaging_data)
+        combined = FullSimulationContainer(simulations, params, mags, chi2)
         return combined
 
 class SimulationOutputContainer(object):
@@ -31,12 +38,13 @@ class SimulationOutputContainer(object):
     It includes the lens data class, the accepted lens system, and the corresponding set of parameters
     """
 
-    def __init__(self, lens_data, lens_system, magnifications, parameters):
+    def __init__(self, lens_data, lens_system, magnifications, parameters, chi2_imaging_data=None):
 
         self.data = lens_data
         self.lens_system = lens_system
         self.parameters = parameters
         self.magnifications = magnifications
+        self.chi2_imaging_data = chi2_imaging_data
 
 def filenames(output_path, job_index):
     """
@@ -53,7 +61,8 @@ def filenames(output_path, job_index):
     filename_acceptance_ratio = output_path + 'job_' + str(job_index) + '/acceptance_ratio.txt'
     return filename_parameters, filename_mags, filename_realizations, filename_sampling_rate, filename_acceptance_ratio
 
-def compile_output(output_path, job_index_min, job_index_max, keep_realizations=False):
+def compile_output(output_path, job_index_min, job_index_max, keep_realizations=False, record_chi2=False,
+                   filename_suffix_chi2=None):
 
     """
     This function complies the result from a simulation into a single pickled python class
@@ -63,6 +72,11 @@ def compile_output(output_path, job_index_min, job_index_max, keep_realizations=
     :param job_index_max: the ending index for output folders
     :param keep_realizations: bool, whether or not to store the accepted realizations and the full lens system for each
     set of accepted parameters
+    :param record_chi2: bool, whether or not to search for/save the chi2 fit to imaging data for each realization.
+    NOTE - if the chi2 file is searched for but not found, then the corresponding flux ratios and parameters
+    will not be saved
+    :param filename_suffix_chi2: an optional string to append on a filename; format is
+    'output_foler/job_$job_index$/chi2_$index$_$filename_suffix$.txt'
     :return: an instance of FullSimulationContainer
     """
 
@@ -71,7 +85,7 @@ def compile_output(output_path, job_index_min, job_index_max, keep_realizations=
     else:
         realizations_and_lens_systems = None
 
-    params, fluxes = None, None
+    params, fluxes, chi2_imaging_data = None, None, None
     n_kept = 0
     for job_index in range(job_index_min, job_index_max + 1):
 
@@ -88,6 +102,18 @@ def compile_output(output_path, job_index_min, job_index_max, keep_realizations=
         except:
             print('could not find file ' + filename_mags)
             continue
+        if record_chi2:
+            _chi2 = []
+            for n in range(0, int(_params.shape[0])):
+                filename_chi2 = output_path + 'job_' + str(job_index) + \
+                                '/chi2_'+ str(n) + filename_suffix_chi2 +'.txt'
+                try:
+                    __chi2 = np.loadtxt(filename_chi2)
+                except:
+                    print('could not find chi2 file ' + filename_chi2)
+                    continue
+                _chi2.append(__chi2)
+            _chi2 = np.array(_chi2)
 
         number = _fluxes.shape[0]
 
@@ -105,6 +131,8 @@ def compile_output(output_path, job_index_min, job_index_max, keep_realizations=
         if params is None:
             params = deepcopy(_params)
             fluxes = deepcopy(_fluxes)
+            if record_chi2:
+                chi2_imaging_data = deepcopy(_chi2)
         else:
             if params.shape[1] != _params.shape[1]:
                 print('shape mismatch on file '+str(job_index))
@@ -115,9 +143,10 @@ def compile_output(output_path, job_index_min, job_index_max, keep_realizations=
                 continue
             params = np.vstack((params, _params))
             fluxes = np.vstack((fluxes, _fluxes))
+            chi2_imaging_data = np.append(chi2_imaging_data, _chi2)
             n_kept += _params.shape[0]
     print('compiled ' + str(n_kept) + ' realizations')
-    container = FullSimulationContainer(realizations_and_lens_systems, params, fluxes)
+    container = FullSimulationContainer(realizations_and_lens_systems, params, fluxes, chi2_imaging_data)
     return container
 
 
